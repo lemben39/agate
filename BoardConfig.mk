b55ba8fc76c26045cb66ce9768a62204be019c14
lemben39 <146639154+lemben39@users.noreply.github.com>
2025-06-04 04:55:15 +0200

    b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,13 +1,34 @@
-DEVICE_PATH := device/xiaomi/agate-twrp
+DEVICE_PATH := device/xiaomi/agate
+
+ALLOW_MISSING_DEPENDENCIES := true
+
 
 BOARD_VENDOR := xiaomi
 TARGET_BOOTLOADER_BOARD_NAME := agate
 TARGET_DEVICE := agate
 
 TARGET_ARCH := arm64
-TARGET_ARCH_VARIANT := armv8-2a
-TARGET_CPU_VARIANT := cortex-a76
+TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := cortex-a55
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-2a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a55
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := agate
+TARGET_NO_BOOTLOADER := true
+TARGET_USES_UEFI := true
+
+# Platform
+TARGET_BOARD_PLATFORM := mt6893
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := agate,agatein,amber
 
 BOARD_HAS_NO_SELECT_BUTTON := true
 
