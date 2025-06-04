2f8a0c6bb7a6421c076c4cf77f95c9079d15f663
lemben39 <146639154+lemben39@users.noreply.github.com>
2025-06-04 04:37:09 +0200

    fd

--- vendorsetup.sh
+++
@@ -1,9 +0,0 @@
-#
-# Copyright (C) 2025 The Android Open Source Project
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-add_lunch_combo omni_boot-user
-add_lunch_combo omni_boot-userdebug
-add_lunch_combo omni_boot-eng
-
